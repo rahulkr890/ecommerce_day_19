1ab144b34698af012804956750cad1badaf40396
rahulkr890 <73601985+rahulkr890@users.noreply.github.com>
2022-02-22 22:08:00 +0530

    Initial solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class App extends React.Component {
+  constructor() {
+    super();
+    console.log("Inside constructor method call");
+    this.state = {
+      count: 0
+    };
+
+  }
+  componentDidMount() {
+    console.log("inside componentDidMount method call");
+  }
+  static getDerivedStateFromProps() {
+    console.log("Inside getDerivedStateFromProps method call");
+    return null;
+  }
+  shouldComponentUpdate() {
+    console.log("Inside shouldComponentUpdate method call");
+    return true;
+  }
+  getSnapshotBeforeUpdate() {
+    console.log("Inside getSnapshotBeforeUpdate method call");
+    return null;
+  }
+  componentDidUpdate() {
+    console.log("Inside componentDidUpdate method call");
+  }
+  componentWillUnmount() {
+    console.log("Inside componentWillUnmount method call");
+  }
+  render() {
+    console.log("Inside render method call");
+    return (
+      <div style={{textAlign:"center"}}>
+        <h1>React lifecycle example</h1>
+        <button
+          onClick={() => {
+            this.shouldComponentUpdate();
+            this.setState({count:this.state.count+1})
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Call componentWillUnmount() method
+        </button>
+      </div>
+    );
+  }
 }
-
-export default App;
